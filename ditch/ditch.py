d157dc0e0e169493ac523c8e6677d848b24f3c77
Lovre Mrčela <lovre.mrcela@fer.hr>
2019-08-09 22:26:15 +0200

    Solved 'ditch'.

--- ditch/ditch.py
+++ ditch/ditch.py
@@ -8,7 +8,7 @@ MAX = int(1e8)
 
 def dijkstra_step():
     visited = [False for _ in range(m)]
-    comes = [None for _ in range(m)]
+    comes = [(None, None) for _ in range(m)]
     max_flow = [0 for _ in range(m)]
     max_flow[inode] = MAX
 
@@ -25,31 +25,33 @@ def dijkstra_step():
 
         visited[cand_node] = True
 
-        for i, dist in enumerate(adj[cand_node]):
-            if dist == 0: continue
-            flow = min(cand_flow, adj[cand_node][i])
-            if max_flow[i] < flow:
-                max_flow[i] = flow
-                comes[i] = cand_node
+        for i, dists in enumerate(adj[cand_node]):
+            for j, dist in enumerate(dists):
+                if dist <= 0: continue
+                flow = min(cand_flow, dist)
+                if max_flow[i] < flow:
+                    max_flow[i] = flow
+                    comes[i] = cand_node, j
 
     flow = max_flow[onode]
     curr = onode
 
     while True:
-        prev = comes[curr]
+        prev, canal = comes[curr]
         if prev is None: break
-        adj[prev][curr] -= flow
-        adj[curr][prev] += flow
+        adj[prev][curr][canal] -= flow
+        adj[curr][prev][canal] += flow
         curr = prev
 
     return flow
 
 with open("ditch.in", "r") as fin, open("ditch.out", "w") as fout:
     n, m = [int(x) for x in fin.readline().split()]
-    adj = [[0 for _ in range(m)] for _ in range(m)]
+    adj = [[[] for _ in range(m)] for _ in range(m)]
     for _ in range(n):
         a, b, flow = [int(x) for x in fin.readline().split()]
-        adj[a - 1][b - 1] = flow
+        adj[a - 1][b - 1].append(flow)
+        adj[b - 1][a - 1].append(0)
 
     inode = 0
     onode = m - 1
